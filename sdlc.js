b6d0aaa603e8eafa36947ab23638b0e7b1b24fa2
Jonathan Nielsen <larsnielsenj@gmail.com>
2022-09-30 10:14:02 -0600

    First Javascript file is finished.

---
+++ sdlc.js
@@ -0,0 +1,28 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+console.log("check SDLF slides")
+
+// Planning
+// Analysis of Requirements  
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+//Although there is some question as to whether Analysis or planning first
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisReq ="This step will fill out what the end user will need, as well as inputs and outputs."
+const design = "This step pseudocodes the project, sets up control flows and functions."
+const implementation = "This step transltes pseudo code into code"
+const testingAndInt = "This step corrects errors and connects all pieces of the code to each other."
+const maintain = "Begin using the code as ewll as well identify and correct errors in the code, to iteratively push towards an ideal implementation"
